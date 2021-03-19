4a1bc28f80d8daf8622fa0ae4b97d4834910d945
Student <student@g006-06.rz.fh-ingolstadt.de>
2021-03-19 15:09:25 +0100

    Aufgabe 1

---
+++ rollaball/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    //Atributes
+    public bool keyboard;
+    public float speed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (keyboard)
+        {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rb.AddForce(movement * speed);
+        }
+
+
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other. gameObject . CompareTag ("Pick Up"))
+        {
+            other. gameObject . SetActive (false);
+        }
+    }
+}
