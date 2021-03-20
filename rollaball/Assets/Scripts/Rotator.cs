8f1330b7d1e390951253be76a513f42a8cf0c667
Alex <greyshark.ah@googlemail.com>
2021-03-20 23:32:08 +0100

    updated scenes and movement of pickups

--- rollaball/Assets/Scripts/Rotator.cs
+++ rollaball/Assets/Scripts/Rotator.cs
@@ -5,34 +5,32 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     //private Vector3 target = new Vector3(0f, 0.05f, 0f);
-    private Vector3 target;
-    private Vector3 initialPos;
+    public Vector3 pos1;
+    public Vector3 pos2;
+    public bool move;
     // Start is called before the first frame update
     void Start()
     {
-        System.Random r = new System.Random();
-        int xInt = r.Next(0, 5);
-        int zInt = r.Next(0,5);
-        target = new Vector3(transform.position.x + 1, 0.05f, transform.position.z);
-        initialPos = transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         transform .Rotate (new Vector3 (15, 30, 45) * Time. deltaTime );
-        float step = 0.1f * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position,target , step);
-
-        if (Vector3.Distance(transform.position, target) < 0.001f)
+        if (move)
         {
-            // Swap the position of the cylinder.
-            Vector3 a = target;
-            Vector3 b = initialPos;
-           target = b;
-           initialPos = a;
-        }
+            float step = 0.1f * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, pos1, step);
 
-    
+            if (Vector3.Distance(transform.position, pos1) < 0.001f)
+            {
+                // Swap the position of the cylinder.
+                Vector3 a = pos1;
+                Vector3 b = pos2;
+                pos1 = b;
+                pos2 = a;
+            }
+        }
     }
 }
