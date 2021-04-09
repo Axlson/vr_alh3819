ec801a8dbbf33f3fe21bd656d4dedf001327012d
Student <student@g006-06.rz.fh-ingolstadt.de>
2021-04-09 16:47:13 +0200

    added joystick controll and hud

---
+++ flightsimulation/Assets/Scripts/DisplayFlightData.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DisplayFlightData : MonoBehaviour
+{
+    public Text textSpeed;
+    public Text textHeight;
+    public GameObject player;
+    public float warningHeight;
+    public GameObject warnImage;
+    private FlyJoystick flyJoystick;
+    private IEnumerator coroutine;
+    private bool showWarn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flyJoystick = player.GetComponent<FlyJoystick>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        textHeight.text = "Alt: " + (int) player.transform.position.y + " m";
+        textSpeed.text = "Speed: " + (int)(flyJoystick.speed * flyJoystick.speedAmplifier)  + " km/h";
+
+        if (player.transform.position.y < warningHeight)
+        {
+            warnImage.SetActive(true);
+        }else 
+        {
+            warnImage.SetActive(false);
+        }
+        //StartCoroutine("BlinkWarning", 5.0f);
+
+        
+ 
+
+}
+
+private IEnumerator BlinkWarning(float waitTime)
+{
+    while(showWarn)
+    {
+        warnImage.SetActive(true);
+        Debug.Log("Entered BlinkWarning");
+        yield return new WaitForSeconds(waitTime);
+        warnImage.SetActive(false);
+        yield return new WaitForSeconds(waitTime);
+    }
+}
+
+
+    }
+
---
+++ flightsimulation/Assets/Scripts/FlyJoystick.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyJoystick : MonoBehaviour
+{
+    public float speed;
+    public GameObject terrain;
+    public float speedAmplifier;
+    public float rotationSpeed;
+    private float inputPitch;
+    private float inputYawn;
+    private float inputRoll;
+    public GameObject spotLight;
+    private Vector3 forward = new Vector3(1,0,0);
+    
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputPitch = Input.GetAxis("Pitch");
+        inputYawn = Input.GetAxis("Yawn");
+        inputRoll = Input.GetAxis("Roll");
+        speed = Input.GetAxis("Throttle");
+        Vector3 rotVec = new Vector3(inputRoll * Time.deltaTime * rotationSpeed, inputYawn * Time.deltaTime * rotationSpeed, inputPitch * Time.deltaTime * rotationSpeed);
+        
+        this.transform.Rotate(rotVec);
+        this.transform.Translate (forward * speed * speedAmplifier * Time.deltaTime);
+
+        if (Input.GetButton("Fire1"))
+        {
+            spotLight.SetActive(true);
+        } else{
+            spotLight.SetActive(false);
+        }
+        
+    }
+}
