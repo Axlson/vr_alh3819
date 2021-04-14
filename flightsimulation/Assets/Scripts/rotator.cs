a40ad5e056f2c0727174973185356c0f0fcee01b
Alex <greyshark.ah@googlemail.com>
2021-04-14 15:45:47 +0200

    added textured terrain and dust in inspektion

--- flightsimulation/Assets/Scripts/rotator.cs
+++ flightsimulation/Assets/Scripts/rotator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class rotator : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
+        transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime * speed);
     }
 }
